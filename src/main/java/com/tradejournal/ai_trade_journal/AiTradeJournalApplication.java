15fa51bf96195712a569774bd0d7116669c7c0d4
Rahul Kumar <rahulkumar@Rahuls-MacBook-Air.local>
2026-08-26 13:29:28 +0530

    Add Spring Boot project scaffold

---
+++ src/main/java/com/tradejournal/ai_trade_journal/AiTradeJournalApplication.java
@@ -0,0 +1,13 @@
+package com.tradejournal.ai_trade_journal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTradeJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiTradeJournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tradejournal/ai_trade_journal/AiTradeJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradejournal.ai_trade_journal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiTradeJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
